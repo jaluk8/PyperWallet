c27f399ce01d18d68f9d9e6cddad9a71b73b3acc
Jan Krueger <jaluk8@live.com>
2017-12-20 00:10:34 -0800

    Added KeyPairs without private keys. - Given only a public key but no private key, KeyPairs can be made. - Tge program can currently not turn a compressed into an uncompressed key.

--- pyperlib/ec.py
+++ pyperlib/ec.py
@@ -5,40 +5,60 @@ from pyperlib import data
 SECP256K1 = ec.SECP256K1
 
 class KeyPair:
-    def __init__(self, curve, priv=None):
+    def __init__(self, curve, *, priv=None, pub=None):
         self.curve = curve()
-        if priv is None:
-            self.gen()
+        self.priv = None
+        self.pub_u = None
+        self.pub_c = None
+        
+        if priv is not None:
+            self.load_priv(priv)
+        elif pub is not None:
+            self.load_pub(pub)
         else:
-            self.load(priv)
+            self.gen()
 
     def gen(self):
         self.priv = data.Data(b'\x00')
         while self.priv.bytes[0] == 0: # Some coins (ETH) do not allow leading zeroes in private keys.
             key = ec.generate_private_key(self.curve, default_backend())
-            numbers = key.private_numbers()
-            self.set_privnumbers(numbers)
+            self.set_priv(key)
 
-    def load(self, priv):
+    def load_priv(self, priv):
         key = ec.derive_private_key(priv.int, self.curve, default_backend())
-        numbers = key.private_numbers()
-        self.set_privnumbers(numbers)
+        self.set_priv(key)
 
-    def set_privnumbers(self, privnumbers):
+    def load_pub(self, pub):
+        t = pub[0].hex
+        if t == '04':
+            l = len(pub) // 2 + 1
+            x = pub[1:l].int
+            y = pub[l:].int
+            self.set_pub(x, y)
+        else:
+            self.pub_c = pub
+
+    def set_priv(self, priv):
+        privnumbers = priv.private_numbers()
         self.private_numbers = privnumbers
         self.public_numbers = privnumbers.public_numbers
-        
+
         priv_int = privnumbers.private_value
         self.priv = data.Data.fromint(priv_int, self.curve.key_size//8)
 
         x_int = self.public_numbers.x
         y_int = self.public_numbers.y
 
-        coord_size = (self.curve.key_size//8 - 1) // 2
-        x_data = data.Data.fromint(x_int, coord_size)
-        y_data = data.Data.fromint(y_int, coord_size)
+        self.set_pub(x_int, y_int)
 
-        if y_int % 2 == 0:
+        assert len(self.priv.bytes) == self.curve.key_size//8
+
+    def set_pub(self, x, y):
+        coord_size = (self.curve.key_size//8 - 1) // 2
+        x_data = data.Data.fromint(x, coord_size)
+        y_data = data.Data.fromint(y, coord_size)
+
+        if y % 2 == 0:
             prefix = b'\x02'
         else:
             prefix = b'\x03'
@@ -46,9 +66,7 @@ class KeyPair:
         uncompressed = data.Data(b'\x04') + x_data + y_data
         compressed = data.Data(prefix) + x_data
         self.pub_u = uncompressed
-        self.pub_c = compressed
-        
-        assert len(self.priv.bytes) == self.curve.key_size//8
+        self.pub_c = compressed        
 
     def pub(self, compressed):
         if compressed:
--- tests/test_ec.py
+++ tests/test_ec.py
@@ -2,9 +2,14 @@ from unittest import TestCase
 from pyperlib import ec
 from pyperlib import data
 
-class Test(TestCase):
+ex_priv = data.Data.fromhex('AEFA9CD9F498E6E0929277CD19FE8699D343582A6A6C5C8A79C6D76444DCE942')
+ex_pubu = data.Data.fromhex('040BF82C0EC3CB07FE72F5DCB264624A9CF775CC996B64778C6FA7E1462C119B'+
+                            'D3FA2E796B504AA3FCF4D4022D107858D988D4793EBE538670877C8F72E438FDC7')
+ex_pubc = data.Data.fromhex('030BF82C0EC3CB07FE72F5DCB264624A9CF775CC996B64778C6FA7E1462C119BD3')
+
+class TestPriv(TestCase):
     def load(self, curve, priv, pub_u, pub_c):
-        e = ec.KeyPair(curve, priv)
+        e = ec.KeyPair(curve, priv=priv)
         self.assertEqual(type(e.priv), data.Data)
         self.assertEqual(type(e.pub_u), data.Data)
         self.assertEqual(type(e.pub_c), data.Data)
@@ -20,8 +25,28 @@ class Test(TestCase):
     def test_set(self):
         for x in range(100):
             self.gen(ec.SECP256K1)
-        self.load(ec.SECP256K1,
-                  data.Data.fromhex('AEFA9CD9F498E6E0929277CD19FE8699D343582A6A6C5C8A79C6D76444DCE942'),
-                  data.Data.fromhex('040BF82C0EC3CB07FE72F5DCB264624A9CF775CC996B64778C6FA7E1462C119B'+
-                                    'D3FA2E796B504AA3FCF4D4022D107858D988D4793EBE538670877C8F72E438FDC7'),
-                  data.Data.fromhex('030BF82C0EC3CB07FE72F5DCB264624A9CF775CC996B64778C6FA7E1462C119BD3'))
+        self.load(ec.SECP256K1, ex_priv, ex_pubu, ex_pubc)
+
+class TestPubU(TestCase):
+    def load_u(self, curve, pub_u, pub_c):
+        e = ec.KeyPair(curve, pub=pub_u)
+        self.assertEqual(e.priv, None)
+        self.assertEqual(type(e.pub_u), data.Data)
+        self.assertEqual(type(e.pub_c), data.Data)
+
+        self.assertEqual(e.pub_u, pub_u)
+        self.assertEqual(e.pub_c, pub_c)
+
+    def test_set(self):
+        self.load_u(ec.SECP256K1, ex_pubu, ex_pubc)
+
+class TestPubC(TestCase):
+    def load_c(self, curve, pub_c):
+        e = ec.KeyPair(curve, pub=pub_c)
+        self.assertEqual(e.priv, None)
+        self.assertEqual(e.pub_u, None)
+        self.assertEqual(type(e.pub_c), data.Data)
+        self.assertEqual(e.pub_c, pub_c)
+
+    def test_set(self):
+        self.load_c(ec.SECP256K1, ex_pubc)
