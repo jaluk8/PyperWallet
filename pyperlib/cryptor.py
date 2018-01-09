e4eadffaa112af464797abc22e6eb0b3c6c4244d
Jan Krueger <jaluk8@live.com>
2018-01-09 00:11:19 -0800

    Refactored the BIP38Cryptor class. - Prevented some code reuse through functions. - Also added a check whether the password is incorrect.

--- pyperlib/cryptor.py
+++ pyperlib/cryptor.py
@@ -1,4 +1,9 @@
 from pyperlib import mods, helper, data
+from copy import deepcopy
+
+
+class DecryptionUnsucessfulError(Exception):
+    """Raised when a password is wrong or decryption fails otherwise."""
 
 
 class CryptorFactory(helper.NameFactory):
@@ -12,14 +17,15 @@ class BaseCryptor:
 
     name = "identity"
 
-    def __init__(self):
-        """Cryptors require no initialization right now."""
+    def __init__(self, prompt=None):
+        """Create a cryptor with a prompter for getting passwords."""
+        self.prompt = prompt
 
     def encrypt(self, coin, **kwargs):
-        """Return the wif encrypted with no encryption."""
+        """Encrypt with nothing."""
 
     def decrypt(self, coin, **kwargs):
-        """Return the encrypted wif as an unencrypted type."""
+        """Decrypt with nothing."""
 
 
 class BIP38Cryptor(BaseCryptor):
@@ -27,16 +33,27 @@ class BIP38Cryptor(BaseCryptor):
 
     name = "bip38"
 
-    def encrypt(self, coin, passphrase):
-        """Return the wif encrypted with bip38."""
+    def base58check(self, d):
+        """Return the base58checksum of the given data."""
+        return mods.Sha256(mods.Sha256(d))[:4]
+
+    def addr_hash(self, coin):
+        """Compute a BIP38 address hash from the coin's addr."""
+        addr_string = data.StringData(coin.addr_string())
+        return self.base58check(addr_string)
+
+    def encrypt(self, coin, passphrase=None):
+        """Encrypt with bip38, mutating coin.wif."""
+        if passphrase is None:
+            passphrase = self.prompt.prompt_pass("BIP38 Password")
+
         prefix = data.HexData("0142")
         if coin.settings.compression:
             flag = data.HexData("E0")
         else:
             flag = data.HexData("C0")
 
-        addr_string = data.StringData(coin.addr_string())
-        addr_hash = mods.Sha256(mods.Sha256(addr_string))[:4]
+        addr_hash = self.addr_hash(coin)
 
         passphrase = data.StringData(passphrase)
         key = mods.Scrypt(passphrase, addr_hash)
@@ -51,10 +68,13 @@ class BIP38Cryptor(BaseCryptor):
         enc_h2 = mods.Aes256Enc(block2, key_h2)
 
         enc_wif = prefix + flag + addr_hash + enc_h1 + enc_h2
-        coin.wif = coin.base58check(enc_wif)
+        coin.wif = enc_wif + self.base58check(enc_wif)
 
-    def decrypt(self, coin, passphrase):
-        """Return the encrypted wif as bip38."""
+    def decrypt(self, coin, passphrase=None):
+        """Decrypt with bip38, loading the new private key."""
+        if passphrase is None:
+            passphrase = self.prompt.prompt_pass("BIP38 Password", repeat=False)
+
         addr_hash = coin.wif[3:7]
 
         passphrase = data.StringData(passphrase)
@@ -72,4 +92,13 @@ class BIP38Cryptor(BaseCryptor):
         priv_h1 = mods.Xor(block1, key_h1[:16])
         priv_h2 = mods.Xor(block2, key_h1[16:])
 
-        coin.load_priv(priv_h1 + priv_h2)
+        priv = priv_h1 + priv_h2
+
+        coin_test = deepcopy(coin)
+        coin_test.load_priv(priv)
+        addr_hash2 = self.addr_hash(coin_test)
+
+        if addr_hash == addr_hash2:
+            coin.load_priv(priv)
+        else:
+            raise DecryptionUnsucessfulError("Password incorrect")
